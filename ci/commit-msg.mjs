80f21134ffb5bc7c9497df2afead384c0f4fc879
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-10-01 14:58:01 -0700

    ci: Update commit comment linting

--- ci/commit-msg.mjs
+++ ci/commit-msg.mjs
@@ -1,5 +1,3 @@
-// Invoked on the commit-msg git hook by simple-git-hooks.
-
 import { readFileSync } from 'fs';
 import colors from 'picocolors';
 
@@ -14,21 +12,21 @@ const commitRE =
 if (!commitRE.test(commitTitle)) {
   console.log();
   console.error(
-    `  ${colors.bgRed(colors.white(' ERROR '))} ${colors.white(
+    `${colors.bgRed(colors.white(' ERROR '))} ${colors.white(
       `Invalid commit title, format, or length.`
     )}\n\n` +
       colors.white(
-        `  Commit messages must under 100 characters and have the following format:\n\n`
+        `Please ensure your commit message follows these guidelines:\n`
       ) +
-      `    ${colors.green(
-        `- Starts with feat, fix, refactor, test, perf, style, asset, doc, ci, chore or wip`
-      )}\n` +
-      `    ${colors.green(
-        `- Followed by a semi-colon and a space, then a capitalized title.`
+      `  ${colors.green(`1. Under 100 characters in length.`)}\n` +
+      `  ${colors.green(`2. Starts with one of the following prefixes:`)}\n` +
+      `     - feat, fix, refactor, test, perf, style, asset, doc, ci, chore, or wip\n` +
+      `  ${colors.green(`3. Followed by a semi-colon and a space.`)}\n` +
+      `  ${colors.green(
+        `4. A capitalized title that does not end with a period.`
       )}\n\n` +
-      `    ${colors.white(
-        `Example => ${colors.green(`feat: Add new cool feature`)}`
-      )}\n\n`
+      colors.white(`Example:`) +
+      `\n  ${colors.cyan(`feat: Add new cool feature`)}\n\n`
   );
   process.exit(1);
 }
