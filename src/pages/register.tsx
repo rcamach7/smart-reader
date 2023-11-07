32f154b74cbfcd08478d7b9f79e47c1ac23aa580
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-11-06 20:03:11 -0800

    🚧 wip: Create basic register page

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,3 +1,45 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 export default function Home() {
-  return <h1>Register Page</h1>;
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: '',
+  });
+
+  function handleSubmit(e) {
+    axios.post('/register', credentials);
+  }
+
+  function handleChange(e) {
+    setCredentials({
+      ...credentials,
+      [e.target.id]: e.target.value,
+    });
+  }
+
+  useEffect(() => {
+    console.log(credentials);
+  }, [credentials]);
+
+  return (
+    <div>
+      <h1>Register</h1>
+      <form action="put" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="username"
+          value={credentials.username}
+          onChange={handleChange}
+        />
+        <input
+          id="password"
+          type="password"
+          onChange={handleChange}
+          value={credentials.password}
+        />
+        <input type="submit" />
+      </form>
+    </div>
+  );
 }
