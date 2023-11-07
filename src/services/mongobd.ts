3daccf4e179aab27b086cdbedde60d773f812560
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-11-06 18:16:03 -0800

    🚀 perf: Cache connection to manage open connections with DB

--- src/services/mongobd.ts
+++ src/services/mongobd.ts
@@ -34,7 +34,30 @@ async function connectToMongoDB() {
     throw new Error('Please add your Mongo URI to .env.local');
   }
 
-  return mongoose.connect(uri);
+  let cached = global.mongoose;
+  if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null };
+  }
+
+  async function dbConnect() {
+    if (cached.conn) {
+      return cached.conn;
+    }
+
+    if (!cached.promise) {
+      const opts = {
+        bufferCommands: false,
+      };
+
+      cached.promise = mongoose.connect(uri, opts).then((mongoose) => {
+        return mongoose;
+      });
+    }
+    cached.conn = await cached.promise;
+    return cached.conn;
+  }
+
+  return dbConnect;
 }
 
 export { mongodb_connection_promise, connectToMongoDB };
