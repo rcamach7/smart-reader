3328fe077aaa52b7bc6c4bb431cb7cbbb8bfa039
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-11-07 16:31:03 -0800

    ♻️ refactor: Make index page the auth test page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,25 @@
+import { useSession, signIn, signOut } from 'next-auth/react';
+import { useEffect } from 'react';
+
 export default function Index() {
-  return <>Home Page</>;
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    console.log(session);
+  }, [session]);
+
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  );
 }
