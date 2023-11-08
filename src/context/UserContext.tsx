66cce63d9e67a70107071c548d57ea59b9fae31d
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-11-07 21:52:23 -0800

    ✨ feat: Create User Context

---
+++ src/context/UserContext.tsx
@@ -0,0 +1,62 @@
+import React, {
+  createContext,
+  useState,
+  useContext,
+  ReactNode,
+  useEffect,
+} from 'react';
+import axios from 'axios';
+
+interface User {
+  _id: string;
+  name: string;
+}
+
+interface UserContextType {
+  user: User | null;
+  setUser: React.Dispatch<React.SetStateAction<User | null>>;
+  loadUser?: () => void;
+}
+
+const UserContext = createContext<UserContextType | undefined>(undefined);
+
+export function useUser() {
+  const context = useContext(UserContext);
+  if (context === undefined) {
+    throw new Error('useUser must be used within a UserProvider');
+  }
+  return context;
+}
+
+interface UserProviderProps {
+  children: ReactNode;
+}
+
+export function UserProvider({ children }: UserProviderProps) {
+  const [user, setUser] = useState<User | null>(null);
+
+  async function fetchUser() {
+    try {
+      const res = await axios.get('/api/auth/user');
+      return res.data.user;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
+  async function loadUser() {
+    const userData: User = await fetchUser();
+    setUser(userData);
+  }
+
+  useEffect(() => {
+    loadUser();
+  }, []);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
---
+++ src/pages/api/auth/user.ts
@@ -0,0 +1,46 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { UserSchema } from '@/schemas/index';
+import { connectToMongoDB } from '@/services/mongobd';
+import jwt from 'jsonwebtoken';
+import { parse } from 'cookie';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { token } = parse(req.headers.cookie || '');
+  if (!token) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
+
+  let decoded;
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+
+  switch (req.method) {
+    case 'GET':
+      await connectToMongoDB();
+
+      try {
+        const user = await UserSchema.findById(decoded._id);
+        const { password, ...userWithoutPassword } = user;
+
+        return res.status(200).json({ user: userWithoutPassword });
+      } catch (error) {
+        res.setHeader(
+          'Set-Cookie',
+          'token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httpOnly;'
+        );
+        return res.status(400).json({ message: 'User not found' });
+      }
+
+      return res.status(201).json({ message: 'Account has been created' });
+
+    default:
+      res.setHeader('Allow', ['POST']);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
