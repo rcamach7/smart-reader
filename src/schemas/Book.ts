a809db3ac33476930ca3b0f866d436424e5a3702
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-11-11 14:27:29 -0800

    ✨ feat: Update book schema with appropriate fields and validation

--- src/schemas/Book.ts
+++ src/schemas/Book.ts
@@ -1,15 +1,48 @@
 import { model, Schema, models } from 'mongoose';
 
-const Book = new Schema({
-  title: String,
-  author: String,
+const BookSchema = new Schema({
+  title: { type: String, required: true },
+  authors: [String],
+  publisher: String,
+  publicationDate: Date,
+  edition: String,
+  description: String,
+  pageCount: { type: Number, default: 0 },
+  coverImageLinks: {
+    small: String,
+    normal: String,
+  },
+  isbn: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return /^(97(8|9))?\d{9}(\d|X)$/.test(v); // Simple ISBN validation
+      },
+      message: (props) => `${props.value} is not a valid ISBN!`,
+    },
+  },
+  isbn13: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return /^(97(8|9))?\d{12}$/.test(v); // Simple ISBN-13 validation
+      },
+      message: (props) => `${props.value} is not a valid ISBN-13!`,
+    },
+  },
   genre: [{ type: Schema.Types.ObjectId, ref: 'Genre' }],
 });
 
-const BookModel = models.Book || model('Book', Book);
+BookSchema.index({ title: 1, authors: 1, isbn: 1 });
+
+const BookModel = models.Book || model('Book', BookSchema);
 
 export const initializeBookSchema = () => {
-  BookModel;
+  try {
+    BookModel;
+  } catch (error) {
+    console.error('Error initializing Book schema:', error);
+  }
 };
 
 export default BookModel;
