12e3f5dc87e8a50e41bdf1a73f6a4c57dab18edb
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-11-14 22:28:04 -0800

    ✨ feat: Add delete and auth checks for shelf document endpoint

--- src/pages/api/shelf/[sid].ts
+++ src/pages/api/shelf/[sid].ts
@@ -1,6 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { ShelfSchema } from '@/schemas/index';
 import { connectToMongoDB } from '@/services/mongobd';
+import { parse } from 'cookie';
+import jwt from 'jsonwebtoken';
 
 interface UpdateFields {
   name?: string;
@@ -14,6 +16,20 @@ export default async function handler(
 ) {
   const { sid } = req.query;
   if (!sid) return res.status(400).json({ message: 'Missing shelf id' });
+
+  const { token } = parse(req.headers.cookie || '');
+  let decodedAuthToken;
+  try {
+    decodedAuthToken = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+    res.setHeader(
+      'Set-Cookie',
+      'token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httpOnly;'
+    );
+    return res
+      .status(401)
+      .json({ message: 'Invalid token, please login again.' });
+  }
 
   switch (req.method) {
     case 'GET':
@@ -52,6 +68,13 @@ export default async function handler(
       try {
         await connectToMongoDB();
 
+        const shelf = await ShelfSchema.findById(sid);
+        if (shelf.creator !== decodedAuthToken._id) {
+          return res
+            .status(400)
+            .json({ message: 'Only creator can update this shelf.' });
+        }
+
         const updatedShelf = await ShelfSchema.findByIdAndUpdate(
           sid,
           updateFields,
@@ -67,8 +90,32 @@ export default async function handler(
         return res.status(500).json({ message: 'Error updating shelf', error });
       }
 
+    case 'DELETE':
+      try {
+        await connectToMongoDB();
+
+        const shelf = await ShelfSchema.findById(sid);
+        if (!shelf)
+          return res.status(400).json({ message: 'Unable to find shelf' });
+
+        if (shelf.creator !== decodedAuthToken._id) {
+          return res
+            .status(400)
+            .json({ message: 'Only creator can delete this shelf.' });
+        }
+
+        await ShelfSchema.findByIdAndDelete(sid);
+
+        return res.status(200).json({ message: 'Shelf has been deleted' });
+      } catch (error) {
+        console.log(error);
+        return res
+          .status(500)
+          .json({ message: 'Error retrieving shelf', error });
+      }
+
     default:
-      res.setHeader('Allow', ['GET', 'PUT']);
+      res.setHeader('Allow', ['GET', 'PUT', 'DELETE']);
       res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
