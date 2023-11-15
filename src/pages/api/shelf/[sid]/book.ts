89260ac6c6eb6ec443217f39bcf00aad7e3f6e2b
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-11-15 14:20:52 -0800

    ✨ feat: Add api logic to add and delete book from shelf

--- src/pages/api/shelf/[sid]/book.ts
+++ src/pages/api/shelf/[sid]/book.ts
@@ -1,25 +1,77 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { connectToMongoDB } from '@/services/mongobd';
+import { ShelfSchema } from '@/schemas/index';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const { sid } = req.query;
+  const { bookId } = req.body;
 
-  if (!sid || typeof sid !== 'string') {
-    return res.status(400).json({ message: 'Shelf ID is required' });
+  if (
+    !sid ||
+    typeof sid !== 'string' ||
+    !bookId ||
+    typeof bookId !== 'string'
+  ) {
+    return res
+      .status(400)
+      .json({ message: 'Shelf ID and Book ID are required' });
   }
 
   try {
     switch (req.method) {
-      case 'POST':
-        break;
+      case 'PUT':
+        try {
+          await connectToMongoDB();
+
+          const updatedShelf = await ShelfSchema.findByIdAndUpdate(
+            sid,
+            {
+              $push: { books: bookId },
+            },
+            { new: true }
+          ).populate(['books', 'likes', 'creator']);
+
+          return res.status(200).json({
+            message: 'Added book to shelf.',
+            shelf: updatedShelf.toObject(),
+          });
+        } catch (error) {
+          console.log(error);
+          return res.status(500).json({
+            message: 'Internal error adding book to shelf.',
+            error,
+          });
+        }
 
       case 'DELETE':
-        break;
+        try {
+          await connectToMongoDB();
+
+          const updatedShelf = await ShelfSchema.findByIdAndUpdate(
+            sid,
+            {
+              $pull: { books: bookId },
+            },
+            { new: true }
+          ).populate(['books', 'likes', 'creator']);
+
+          return res.status(200).json({
+            message: 'Deleted book from shelf.',
+            shelf: updatedShelf.toObject(),
+          });
+        } catch (error) {
+          console.log(error);
+          return res.status(500).json({
+            message: 'Internal error deleting book from shelf.',
+            error,
+          });
+        }
 
       default:
-        res.setHeader('Allow', ['POST', 'DELETE']);
+        res.setHeader('Allow', ['PUT', 'DELETE']);
         res.status(405).end(`Method ${req.method} Not Allowed`);
     }
   } catch (error) {
