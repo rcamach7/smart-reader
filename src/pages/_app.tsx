1f4681b1610cb1809e6bfaa1e349591cafd8366c
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-11-18 16:35:38 -0800

    ✨ feat: Create default app theme via mui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,17 @@
 import { UserProvider } from '@/context/UserContext';
 import CssBaseline from '@mui/material/CssBaseline';
 import { AppBar } from '@/components/organisms';
+import theme from '../theme/theme';
+import { ThemeProvider } from '@mui/material/styles';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <UserProvider>
-      <CssBaseline />
-      <AppBar />
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </UserProvider>
   );
 }
