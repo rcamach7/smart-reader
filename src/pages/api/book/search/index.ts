cdd838737a01db03200d6dddec6e4a4cedd9d536
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-11-22 14:30:35 -0800

    ✨ feat: Add types for google books api response

--- src/pages/api/book/search/index.ts
+++ src/pages/api/book/search/index.ts
@@ -1,19 +1,37 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
+import GoogleBooksAPI from '@/services/googleBooksAPI';
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const query = req.query.query as string;
+  const type = req.query.type as string;
+  if (!query || !type) {
+    return res.status(400).json({ message: 'Missing search fields' });
+  } else if (type !== 'query' && type !== 'isbn') {
+    return res.status(400).json({ message: 'Invalid search type' });
+  }
+
   try {
     switch (req.method) {
       case 'GET':
-        break;
-
-      case 'DELETE':
+        try {
+          const booksAPI = new GoogleBooksAPI(process.env.GOOGLE_BOOKS_API_KEY);
+          if (type === 'query') {
+            const response = await booksAPI.findBooksByQuery(query, 2);
+            console.log(response.items[0].volumeInfo.industryIdentifiers);
+            return res.status(200).json(response);
+          }
+        } catch (error) {
+          console.log(error);
+          return res.status(500).json({ message: 'error', error });
+        }
         break;
 
       default:
-        res.setHeader('Allow', ['PUT', 'DELETE']);
+        res.setHeader('Allow', ['GET']);
         res.status(405).end(`Method ${req.method} Not Allowed`);
     }
   } catch (error) {
---
+++ src/services/googleBooksAPI.ts
@@ -0,0 +1,73 @@
+import axios from 'axios';
+
+type Identifier = {
+  type: 'ISBN_10' | 'ISBN_13';
+  identifier: string;
+};
+
+type GoogleBook = {
+  kind: string;
+  id: string;
+  selfLink: string;
+  volumeInfo: {
+    title: string;
+    authors: string[];
+    publisher: string;
+    publishedDate: string;
+    industryIdentifiers: Identifier[];
+    readingModes: { text: boolean; image: boolean };
+    printType: string;
+    categories: string[];
+    imageLinks: {
+      smallThumbnail: string;
+      thumbnail: string;
+    };
+    language: string;
+    previewLink: string;
+    infoLink: string;
+  };
+};
+
+type QueryResponse = {
+  data: {
+    kind: string;
+    totalItems: number;
+    items: GoogleBook[];
+  };
+};
+
+type parameters = 'intitle' | 'inauthor' | 'isbn' | 'insubject' | 'inpublisher';
+
+export default class GoogleBooksAPI {
+  private static _URL = 'https://www.googleapis.com/books/v1';
+  private readonly _apiKey: string;
+
+  constructor(apiKey: string) {
+    this._apiKey = apiKey;
+  }
+
+  async findBooksByQuery(query: string, maxResults?: number) {
+    const endpoint = `${GoogleBooksAPI._URL}/volumes?q=${query}&maxResults=${
+      maxResults ? maxResults : 50
+    }&key=${this._apiKey}`;
+
+    try {
+      const response: QueryResponse = await axios.get(endpoint);
+      return response.data;
+    } catch (error) {
+      console.error('Failed to fetch top headlines:', error);
+      throw new Error('Failed to fetch top headlines.');
+    }
+  }
+
+  async findBookByISBN(query: string) {
+    const endpoint = `${GoogleBooksAPI._URL}/volumes?q=isbn:${query}&key=${this._apiKey}`;
+    try {
+      const response = await axios.get(endpoint);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      throw new Error('Failed to fetch by ISBN');
+    }
+  }
+}
