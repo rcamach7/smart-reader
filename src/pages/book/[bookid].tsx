9d7ee734e5f03c0a838dcb01daf08b5805ad4cbd
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-11-24 15:31:41 -0800

    🎨 style: Create basic layout for book thumbnail and title

--- src/pages/book/[bookid].tsx
+++ src/pages/book/[bookid].tsx
@@ -8,7 +8,7 @@ import Book from '@/types/book';
 import useAvailableHeight from '@/hooks/useAvailableHeight';
 
 import { PageLoading } from '@/components/atoms';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
 export default function CategoryPage() {
   const { setIsPageLoading, isPageLoading } = useLoadingContext();
@@ -38,8 +38,70 @@ export default function CategoryPage() {
 
   if (book) {
     return (
-      <Box sx={{ height: useAvailableHeight }}>
-        <img src={book.imageLinks?.thumbnail} alt={book.title} />
+      <Box
+        sx={{
+          height: useAvailableHeight,
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            p: 3,
+            backgroundColor: '#d3e3f0',
+            justifyContent: 'center',
+          }}
+        >
+          {/* Content Container */}
+          <Box sx={{ display: 'flex' }}>
+            {/* Image Container */}
+            <Box
+              sx={{
+                width: 'clamp(100px, 25vw, 125px)',
+                height: 'auto',
+                mr: 3,
+              }}
+            >
+              <img
+                src={book.imageLinks?.thumbnail}
+                alt={book.title}
+                style={{
+                  width: '100%',
+                  height: '100%',
+                  objectFit: 'fill',
+                  boxShadow: '20px 20px 20px rgba(0, 0, 0, 0.3)',
+                }}
+              />
+            </Box>
+            {/* Text Container */}
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                pt: 0.5,
+                maxWidth: 400,
+              }}
+            >
+              <Typography
+                textAlign="center"
+                sx={{
+                  fontWeight: 'bold',
+                  fontSize: { xs: '1.1rem', sm: '1.2rem', md: '1.3rem' },
+                }}
+              >
+                {book.title}
+              </Typography>
+              <Typography
+                variant="body1"
+                textAlign="center"
+                sx={{ fontSize: { xs: '1rem', sm: '1.1rem', md: '1.2rem' } }}
+              >
+                by {book.authors}
+              </Typography>
+            </Box>
+          </Box>
+        </Box>
       </Box>
     );
   } else if (error) {
