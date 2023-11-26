5128e6018da6dd336ac4d0bb07893b9da35b82d7
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-11-26 12:56:12 -0800

    🚧 wip: Create section header for my library page

--- src/pages/my-library.tsx
+++ src/pages/my-library.tsx
@@ -1,3 +1,66 @@
+import useAvailableHeight from '@/hooks/useAvailableHeight';
+
+import { Box, Button, Typography } from '@mui/material';
+
 export default function MyLibrary() {
-  return <>My Library Page</>;
+  const availableHeight = useAvailableHeight();
+
+  return (
+    <Box
+      sx={{
+        height: availableHeight,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <Box
+        sx={{
+          height: '100%',
+          width: 'clamp(320px, 100vw, 900px)',
+        }}
+      >
+        <Typography textAlign="center" gutterBottom>
+          My Library
+        </Typography>
+
+        <SectionHeader
+          title="My Favorited Books"
+          buttonText="Search For More"
+          handleButtonClick={() => {}}
+        />
+        <Box></Box>
+      </Box>
+    </Box>
+  );
+}
+
+interface Props {
+  title: string;
+  buttonText: string;
+  handleButtonClick: () => void;
+}
+
+function SectionHeader({ title, buttonText, handleButtonClick }: Props) {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        p: 1,
+        boxShadow: '0 6px 4px -6px rgba(0, 0, 0, 0.5)',
+        '&:hover': {
+          boxShadow: '0 8px 6px -6px rgba(0, 0, 0, 0.6)',
+        },
+      }}
+    >
+      <Typography
+        sx={{ fontSize: { xs: '1.1rem', sm: '1.2rem', md: '1.3rem' } }}
+      >
+        {title}
+      </Typography>
+      <Button variant="outlined">{buttonText}</Button>
+    </Box>
+  );
 }
