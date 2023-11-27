bcdc3ae1c4e405d9836f6bac93f240632b365e55
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-11-27 12:23:44 -0800

    ♻️ refactor: Separate containers for books and sheld for toggle display

--- src/pages/my-library.tsx
+++ src/pages/my-library.tsx
@@ -6,14 +6,40 @@ import { Box } from '@mui/material';
 import { BookCard } from '@/components/molecules';
 import { SectionHeader } from '@/components/atoms';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+
+type ViewMode = 'favorites' | 'shelves';
 
 export default function MyLibrary() {
   const availableHeight = useAvailableHeight();
   const router = useRouter();
   const { user, isUserLoading } = useUser();
   const { setIsPageLoading } = useLoadingContext();
+  const [viewMode, setViewMode] = useState<ViewMode>('favorites');
+
+  const toggleViewMode = () => {
+    setViewMode((VM) => {
+      return VM === 'favorites' ? 'shelves' : 'favorites';
+    });
+  };
+
+  const renderFavoritedBooks = (
+    <Box>
+      <SectionHeader
+        title="My Favorited Books"
+        buttonText="Search For More"
+        handleButtonClick={() => {}}
+      />
+      <Box sx={{ p: 1 }}>
+        {user.savedBooks.map((book) => {
+          return <BookCard book={book} type="transparent" />;
+        })}
+      </Box>
+    </Box>
+  );
+
+  const renderShelves = <>Shelves Collection</>;
 
   useEffect(() => {
     if (!user && !isUserLoading) {
@@ -45,16 +71,7 @@ export default function MyLibrary() {
           width: 'clamp(320px, 100vw, 900px)',
         }}
       >
-        <SectionHeader
-          title="My Favorited Books"
-          buttonText="Search For More"
-          handleButtonClick={() => {}}
-        />
-        <Box sx={{ p: 1 }}>
-          {user.savedBooks.map((book) => {
-            return <BookCard book={book} type="transparent" />;
-          })}
-        </Box>
+        {viewMode === 'favorites' ? renderFavoritedBooks : renderShelves}
       </Box>
     </Box>
   );
