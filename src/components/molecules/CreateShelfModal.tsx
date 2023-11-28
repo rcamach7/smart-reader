9a59060299ebeddd7acd16e9c74907face104fc8
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-11-27 17:03:50 -0800

    🚧 wip: Create fields for form

--- src/components/molecules/CreateShelfModal.tsx
+++ src/components/molecules/CreateShelfModal.tsx
@@ -1,4 +1,66 @@
-import { Button, Modal, Typography, Box } from '@mui/material';
+import {
+  Modal,
+  Typography,
+  Box,
+  TextField,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+} from '@mui/material';
+
+interface Props {
+  open: boolean;
+  toggle: () => void;
+}
+
+export default function CreateShelf({ open, toggle }: Props) {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={toggle}
+        aria-labelledby="modal-modal-confirm"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style} component="form" onSubmit={handleSubmit}>
+          <Typography textAlign="center">Create New Shelf</Typography>
+          <TextField
+            id="standard-helperText"
+            label="Shelf Title"
+            variant="standard"
+          />
+          <TextField
+            id="standard-helperText"
+            label="Description"
+            variant="standard"
+            rows={2}
+            multiline
+            maxRows={4}
+          />
+          <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+            <InputLabel id="demo-simple-select-standard-label">
+              Visibility
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-standard-label"
+              id="demo-simple-select-standard"
+              value="public"
+              label="Visibility"
+            >
+              <MenuItem value="public">Public</MenuItem>
+              <MenuItem value="private">Private</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
 
 const style = {
   position: 'absolute' as 'absolute',
@@ -9,24 +71,6 @@ const style = {
   bgcolor: 'background.paper',
   boxShadow: 24,
   p: 2,
+  display: 'flex',
+  flexDirection: 'column',
 };
-
-interface Props {
-  open: boolean;
-  toggle: () => void;
-}
-
-export default function CreateShelf({ open, toggle }: Props) {
-  return (
-    <div>
-      <Modal
-        open={open}
-        onClose={toggle}
-        aria-labelledby="modal-modal-confirm"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>Hello World</Box>
-      </Modal>
-    </div>
-  );
-}
