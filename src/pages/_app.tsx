124d7f9effa4f8c80893fb10f2df3601af2e79cd
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-12-20 16:27:31 -0800

    🎨 style: Add favicons to all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
+import Head from 'next/head';
 
 import { UserProvider } from '@/context/UserContext';
 import { LoadingProvider } from '@/context/LoadingContext';
@@ -17,6 +18,19 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
       <LoadingProvider>
         <FeedbackProvider>
           <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+            <Head>
+              <link rel="icon" href="/favicons/favicon.ico" />
+              <link
+                rel="icon"
+                href="/favicons/favicon-32x32.png"
+                sizes="32x32"
+              />
+              <link
+                rel="icon"
+                href="/favicons/favicon-16x16.png"
+                sizes="16x16"
+              />
+            </Head>
             <CssBaseline />
             <AppBar toggleTheme={toggleTheme} />
             <Component {...pageProps} />
