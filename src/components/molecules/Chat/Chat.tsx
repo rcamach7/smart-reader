74a3507ad6a36b31e4fcf00f6876ce30308e1c50
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2024-01-09 18:12:07 -0800

    🚧 wip: Add empty modal

--- src/components/molecules/Chat/Chat.tsx
+++ src/components/molecules/Chat/Chat.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-import { Box } from '@mui/material';
+import { Box, Modal, Typography } from '@mui/material';
 import { FabButton } from '@/components/atoms';
 
 interface Props {}
@@ -17,7 +17,31 @@ export default function Chat({}: Props) {
   return (
     <>
       <FabButton onClick={() => toggleChat()} />
-      {chat.open ? <Box>Chat Popup</Box> : null}
+      <div>
+        <Modal
+          open={chat.open}
+          onClose={toggleChat}
+          aria-labelledby="modal-modal-confirm"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-confirm" variant="h6" component="h2">
+              Hello World
+            </Typography>
+          </Box>
+        </Modal>
+      </div>
     </>
   );
 }
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 'clamp(300px, 90vw, 900px)',
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  p: 2,
+};
